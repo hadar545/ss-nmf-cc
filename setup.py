f75908388289287a199a51e2236a7748bc1c33e1
Hadar Mulian <39883265+hadar545@users.noreply.github.com>
2021-01-18 17:30:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='SSNMF',
+    name='SSNMFCC',
     version='1.0',
     packages=[''],
-    url='https://github.com/hadar545/ssnmf',
+    url='https://github.com/hadar545/ss-nmf-cc',
     license='',
     author='Hadar Mulian',
     author_email='hadar.mulian@mail.huji.ac.il',
-    description='semi-supervised nonegative matrix factorization'
+    description='semi-supervised nonegative matrix factorization with convex coefficients'
 )
