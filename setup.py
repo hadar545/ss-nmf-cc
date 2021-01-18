230a4a7d5d8571306403ab79de48e0c960ec7435
Hadar Mulian <39883265+hadar545@users.noreply.github.com>
2021-01-18 17:15:38 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SSNMF',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/hadar545/ssnmf',
+    license='',
+    author='Hadar Mulian',
+    author_email='hadar.mulian@mail.huji.ac.il',
+    description='semi-supervised nonegative matrix factorization'
+)
